aa3524b64ee018f4da155acbfe2b43cf43e95314
colin99d <colin99delahunty@gmail.com>
2022-12-13 11:09:27 -0500

    Added readme

---
+++ README.md
@@ -0,0 +1,8 @@
+# Tauri Web Viewer
+
+To use just run the following:
+- `pip install maturin`
+- `maturin develop`
+
+Note: There is currently an issue if you try to run this inside an X86_64 conda
+environment on an M1 machine.
--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,13 @@
-use wry::{
-application::{
-  event::{Event, StartCause, WindowEvent},
-  event_loop::{ControlFlow, EventLoop},
-  window::WindowBuilder,
-},
-webview::WebViewBuilder,
-};
 use pyo3::prelude::*;
+use std::thread;
+use wry::{
+    application::{
+        event::{Event, StartCause, WindowEvent},
+        event_loop::{ControlFlow, EventLoop},
+        window::WindowBuilder,
+    },
+    webview::WebViewBuilder,
+};
 
 /// Formats the sum of two numbers as string.
 #[pyfunction]
@@ -16,29 +17,30 @@ fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
 
 #[pyfunction]
 fn show_html(file_path: String) -> PyResult<String> {
-  let event_loop = EventLoop::new();
-  let window = WindowBuilder::new()
-    .with_title("Juan Step from the MOON")
-    .build(&event_loop)
-    .unwrap();
-  let _webview = WebViewBuilder::new(window).unwrap()
-    .with_html(&file_path)
-    .unwrap()
-    .build()
-    .unwrap();
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new()
+        .with_title("Juan Step from the MOON")
+        .build(&event_loop)
+        .unwrap();
+    let _webview = WebViewBuilder::new(window)
+        .unwrap()
+        .with_html(&file_path)
+        .unwrap()
+        .build()
+        .unwrap();
 
-  event_loop.run(move |event, _, control_flow| {
-    *control_flow = ControlFlow::Wait;
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = ControlFlow::Wait;
 
-    match event {
-      Event::NewEvents(StartCause::Init) => println!("Wry has started!"),
-      Event::WindowEvent {
-        event: WindowEvent::CloseRequested,
-        ..
-      } => *control_flow = ControlFlow::Exit,
-      _ => (),
-    }
-  });
+        match event {
+            Event::NewEvents(StartCause::Init) => println!("Wry has started!"),
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                ..
+            } => *control_flow = ControlFlow::Exit,
+            _ => (),
+        }
+    });
 }
 
 /// A Python module implemented in Rust.
