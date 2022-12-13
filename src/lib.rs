5c31651e9ea101177be77689daab974919b37e5e
colin99d <colin99delahunty@gmail.com>
2022-12-13 11:20:41 -0500

    Removed example code

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,4 @@
 use pyo3::prelude::*;
-use std::thread;
 use wry::{
     application::{
         event::{Event, StartCause, WindowEvent},
@@ -9,12 +8,7 @@ use wry::{
     webview::WebViewBuilder,
 };
 
-/// Formats the sum of two numbers as string.
-#[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
-}
-
+/// A function to show the provided html in a WRY browser
 #[pyfunction]
 fn show_html(file_path: String) -> PyResult<String> {
     let event_loop = EventLoop::new();
@@ -46,7 +40,6 @@ fn show_html(file_path: String) -> PyResult<String> {
 /// A Python module implemented in Rust.
 #[pymodule]
 fn plotly_wry(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
     m.add_function(wrap_pyfunction!(show_html, m)?)?;
     Ok(())
 }
