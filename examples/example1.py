f1d9eddaf142c532559cdcea61196745040ef675
jose-donato <zmcdonato@gmail.com>
2022-12-13 17:39:50 +0000

    fix: added examples folder

--- examples/example1.py
+++ examples/example1.py
@@ -5,12 +5,6 @@ def readcontents():
     with open('file.html', 'r') as f:
         return f.read()
 
-def wait_for_user_input():
-    while True:
-        message = input('Type html to be viewed in webview:\n> ')
-        p = Process(target=task, args=(message,))
-        p.start()
-
 def task(message):
     plotly_wry.show_html(message)
 
