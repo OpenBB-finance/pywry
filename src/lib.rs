03b7abd1a711907f65eb2a376843951a0e289cf5
colin99d <colin99delahunty@gmail.com>
2022-12-15 16:35:42 -0500

    Basic concept down

--- src/lib.rs
+++ src/lib.rs
@@ -11,14 +11,6 @@ pub mod window;
 
 
 #[pyfunction]
-fn send_html(py: Python<'_>, html: String) -> PyResult<&PyAny> {
-    pyo3_asyncio::async_std::future_into_py(py, async {
-        send_message(html).await;
-        Ok(Python::with_gil(|py| py.None()))
-    })
-}
-
-#[pyfunction]
 fn start() -> PyResult<()> {
     let (sender, receiver) = mpsc::channel();
     start_wry(sender, receiver);
@@ -29,6 +21,5 @@ fn start() -> PyResult<()> {
 #[pymodule]
 fn pywry(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(start, m)?)?;
-    m.add_function(wrap_pyfunction!(send_html, m)?)?;
     Ok(())
 }
