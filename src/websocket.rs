5d51d66b68d545721b7b89c01e35c908a612024c
colin99d <colin99delahunty@gmail.com>
2022-12-19 16:30:16 -0500

    Made sockets open forever

--- src/websocket.rs
+++ src/websocket.rs
@@ -2,46 +2,53 @@ use futures::future;
 use futures::prelude::*;
 use std::io::Error as IoError;
 
+use futures_util::{SinkExt, StreamExt};
+use std::net::SocketAddr;
 use std::sync::mpsc::Sender;
 use tokio::{
     net::{TcpListener, TcpStream},
     task,
 };
-use tokio_tungstenite::{accept_async, tungstenite::error::Error};
+use tokio_tungstenite::{
+    accept_async,
+    tungstenite::{Error, Result},
+};
 
 enum ConnectionError {
     Tungstenite(Error),
     Mspc(String),
 }
 
-async fn handle_connection(
-    sender: Sender<String>,
-    raw_stream: TcpStream,
-) -> Result<(), ConnectionError> {
-    let ws_stream = match accept_async(raw_stream).await {
-        Err(err) => return Err(ConnectionError::Tungstenite(err)),
-        Ok(stream) => stream,
-    };
-    let (_, incoming) = ws_stream.split();
-
-    let mut x = String::new();
-    let broadcast_incoming = incoming
-        .try_filter(|msg| future::ready(!msg.is_close()))
-        .try_for_each(|msg| {
-            x = msg.to_text().unwrap_or_default().to_string();
-            future::ok(())
-        });
-
-    if let Err(error) = broadcast_incoming.await {
-        return Err(ConnectionError::Tungstenite(error));
-    }
-    if !&x.eq("<test>") {
-        println!("Sent response");
-        if let Err(error) = sender.send(x.clone()) {
-            println!("Error sending response");
-            return Err(ConnectionError::Mspc(error.0));
+async fn accept_connection(peer: SocketAddr, stream: TcpStream, sender: Sender<String>) {
+    if let Err(e) = handle_connection(peer, stream, sender).await {
+        match e {
+            Error::ConnectionClosed | Error::Protocol(_) | Error::Utf8 => (),
+            err => println!("Error processing connection: {}", err),
         }
     }
+}
+
+async fn handle_connection(
+    peer: SocketAddr,
+    stream: TcpStream,
+    sender: Sender<String>,
+) -> Result<()> {
+    let mut ws_stream = accept_async(stream).await.expect("Failed to accept");
+
+    println!("New WebSocket connection: {}", peer);
+
+    while let Some(msg) = ws_stream.next().await {
+        let msg = msg?;
+        if msg.is_text() || msg.is_binary() {
+            println!("Message: {}", msg);
+            let msg_str = msg.to_text().unwrap();
+            if !&msg_str.eq("<test>") {
+                sender.send(msg_str.to_string()).unwrap();
+                ws_stream.send(msg).await?;
+            }
+        }
+    }
+
     Ok(())
 }
 
@@ -55,9 +62,12 @@ pub async fn run_server(port: u16, sender: Sender<String>) -> Result<(), IoError
 
     // Let's spawn the handling of each connection in a separate task.
     while let Ok((stream, _)) = listener.accept().await {
-        // How will we return errors inside task::spawn?
-        task::spawn(handle_connection(sender.clone(), stream));
-    }
+        let peer = stream
+            .peer_addr()
+            .expect("connected streams should have a peer address");
+        println!("Peer address: {}", peer);
 
+        tokio::spawn(accept_connection(peer, stream, sender.clone()));
+    }
     Ok(())
 }
