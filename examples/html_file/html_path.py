53f9b9c350127652c8aec8d9379493a70d03afad
teh_coderer <me@tehcoderer.com>
2023-05-07 17:04:14 -0400

    Update html_path.py

--- examples/html_file/html_path.py
+++ examples/html_file/html_path.py
@@ -12,7 +12,7 @@ async def main_loop():
 
 if __name__ == "__main__":
     try:
-        html_path = str((Path(__file__).parent / "example.html").resolve())
+        html_path = Path(__file__).parent / "example.html"
         handler = PyWry()
         handler.send_html(html_path=html_path, title="PyWry Demo")
         handler.start()
